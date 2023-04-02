0b3a99891e56c3aa5ad89a8fa4d0b2ff40c93966
montekrist0 <bogdanovigor1994@mail.ru>
2023-04-02 07:15:37 +0300

    initial dev commit

---
+++ ugc_fastapi/src/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI(
+    title="API для получения UGC",
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+    description="...",
+    version="1.0.0",
+)
+
+
+# app.include_router(, prefix="/api/v1")
+
+
+@app.on_event("startup")
+async def startup(): ...
+
+
+@app.on_event("shutdown")
+async def shutdown(): ...
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
