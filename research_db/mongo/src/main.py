8f4831d09e7b71ba446ebf89ccacd279e8b2cf37
montekrist0 <bogdanovigor1994@mail.ru>
2023-04-06 22:18:14 +0300

    add abstract class for dbmanager and mongo realisation

--- research_db/mongo/src/main.py
+++ research_db/mongo/src/main.py
@@ -1,16 +1,68 @@
-from pymongo import MongoClient
 import datetime
+from abc import ABC, abstractmethod
 
-client = MongoClient('mongodb://root:example@localhost:27017/')
+import pymongo.database
+from pymongo import MongoClient
+from pymongo.results import InsertOneResult, InsertManyResult
+from pymongo.errors import PyMongoError
+from pymongo.database import Database, Collection
 
-db = client.test_database
-collection = db.test_collection
+connection_string = "mongodb://root:example@localhost:27017/"
 
-post = {"author": "Mike",
-        "text": "My first blog post!",
-        "tags": ["mongodb", "python", "pymongo"],
-        "date": datetime.datetime.utcnow()}
 
-posts = db.posts
-post_id = posts.insert_one(post).inserted_id
+class DBManager(ABC):
 
+    @abstractmethod
+    def create_db(self, db_name: str):
+        pass
+
+    @abstractmethod
+    def create_collection(self, *params):
+        pass
+
+    @abstractmethod
+    def insert_one(self, collection_name, db_name, doc):
+        pass
+
+    @abstractmethod
+    def insert_many(self, collection_name, db_name):
+        pass
+
+
+class MongoDBManager(DBManager):
+
+    def __init__(self, con_string: str) -> None:
+        self.client = MongoClient(con_string)
+
+    def create_db(self, db_name: str) -> None | Database:
+        return self.client[db_name]
+
+    def create_collection(self, collection_name: str, db_name: str) -> None | Collection:
+        return self.client[db_name][collection_name]
+
+    def insert_one(self, collection_name: str, db_name: str, doc: dict) -> None | InsertOneResult:
+        try:
+            result = self.client[db_name][collection_name].insert_one(doc)
+            return result
+
+        except PyMongoError:
+            return None
+
+    def insert_many(self, collection_name, db_name):
+        pass
+
+
+# client = MongoClient()
+
+# db = client.test_database
+# collection = db.test_collection
+
+# post = {
+#     "author": "Mike",
+#     "text": "My first blog post!",
+#     "tags": ["mongodb", "python", "pymongo"],
+#     "date": datetime.datetime.utcnow(),
+# }
+
+# posts = db.posts
+# post_id = posts.insert_one(post).inserted_id
